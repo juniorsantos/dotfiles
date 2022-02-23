6103d63f85673c17f056b0c86f0f77f8c1012844
J.S.Júnior <j.s.junior@live.com>
2022-02-23 01:01:40 -0300

    my dotfiles

---
+++ nvim/config/cmp.lua
@@ -0,0 +1,196 @@
+local lspconfig = require('lspconfig')
+
+local cmp = require'cmp'
+
+  local on_attach = function(_, bufnr)
+    require('cmp').on_attach()
+    --- In lsp attach function
+    local map = nvim_buf_set_keymap,
+    map(0, "n", "gr", "<cmd>Lspsaga rename<cr>", {silent = true, noremap = true})
+    map(0, "n", "gx", "<cmd>Lspsaga code_action<cr>", {silent = true, noremap = true})
+    map(0, "x", "gx", ":<c-u>Lspsaga range_code_action<cr>", {silent = true, noremap = true})
+    map(0, "n", "K",  "<cmd>Lspsaga hover_doc<cr>", {silent = true, noremap = true})
+    map(0, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", {silent = true, noremap = true})
+    map(0, "n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", {silent = true, noremap = true})
+    map(0, "n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", {silent = true, noremap = true})
+    map(0, "n", "<C-u>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>")
+    map(0, "n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>")
+  end
+  local servers = {'intelephense', 'tailwindcss', 'html'}
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+    }
+  end
+
+local lsp_symbols = {
+    Text = "   (Text) ",
+    Method = "   (Method)",
+    Function = "   (Function)",
+    Constructor = "   (Constructor)",
+    Field = " ﴲ  (Field)",
+    Variable = "[] (Variable)",
+    Class = "   (Class)",
+    Interface = " ﰮ  (Interface)",
+    Module = "   (Module)",
+    Property = " 襁 (Property)",
+    Unit = "   (Unit)",
+    Value = "   (Value)",
+    Enum = " 練 (Enum)",
+    Keyword = "   (Keyword)",
+    Snippet = "   (Snippet)",
+    Color = "   (Color)",
+    File = "   (File)",
+    Reference = "   (Reference)",
+    Folder = "   (Folder)",
+    EnumMember = "   (EnumMember)",
+    Constant = " ﲀ  (Constant)",
+    Struct = " ﳤ  (Struct)",
+    Event = "   (Event)",
+    Operator = "   (Operator)",
+    TypeParameter = "   (TypeParameter)",
+},
+
+require('lspconfig').intelephense.setup{}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require('lspconfig').html.setup{
+  capabilities = capabilities,
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html", "blade", "antlers" },
+}
+
+require('lspconfig').tailwindcss.setup{}
+
+require('lspconfig').stylelint_lsp.setup{}
+
+cmp.setup({
+    experimental = {
+        ghost_text = true,
+    },
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      },
+     formatting = {
+        format = function(entry, item)
+            item.kind = lsp_symbols[item.kind]
+            item.menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+                neorg = "[Neorg]",
+            })["intelephens"]
+
+            return item
+        end,
+    },
+    documentation = {
+        maxwidth = 120,
+        maxheight = 180,
+    },
+    min_length = 1;
+    preselect = true;
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp', priority = 1000 },
+     -- { name = 'intelephense', priority = 1000 },
+      { name = 'buffer', priority = 500 }, 
+    })
+  })
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+ --   require('lspconfig')['intelephense'].setup {
+   --   capabilities = capabilities
+  -- }
+
+-- update diagnostics icons
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end                                  
+
+  require("trouble").setup {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+
+local lspsaga = require 'lspsaga'
+lspsaga.setup { -- defaults ...
+  debug = false,
+  use_saga_diagnostic_sign = true,
+  -- diagnostic sign
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
+  diagnostic_header_icon = "   ",
+  -- code action title icon
+  code_action_icon = " ",
+  code_action_prompt = {
+    enable = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
+  max_preview_lines = 10,
+  finder_action_keys = {
+    open = "o",
+    vsplit = "s",
+    split = "i",
+    quit = "q",
+    scroll_down = "<C-f>",
+    scroll_up = "<C-b>",
+  },
+  code_action_keys = {
+    quit = "q",
+    exec = "<CR>",
+  },
+  rename_action_keys = {
+    quit = "<C-c>",
+    exec = "<CR>",
+  },
+  definition_preview_icon = "  ",
+  border_style = "single",
+  rename_prompt_prefix = "➤",
+  server_filetype_map = {},
+  diagnostic_prefix_format = "%d. ",
+}
---
+++ nvim/plugins.vim
@@ -0,0 +1,133 @@
+" ------------------------------------------------------------------------------
+" # Plugin Definitions
+" ------------------------------------------------------------------------------
+
+    " NERDTree 
+    Plug 'preservim/nerdtree' 
+    Plug 'Xuyuanp/nerdtree-git-plugin'
+    Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
+
+    " Auto pairs for '(' '[' '{'
+    Plug 'jiangmiao/auto-pairs'
+    Plug 'tpope/vim-fugitive'
+    Plug 'tpope/vim-unimpaired'
+
+    " PHP Syntax
+    Plug 'StanAngeloff/php.vim'
+
+    " Blade Syntax
+    Plug 'jwalton512/vim-blade'
+
+    " Testing
+    Plug 'vim-test/vim-test'
+
+    " Tinkeray
+    Plug 'jesseleite/vim-tinkeray'
+
+    " Vim Sourcery
+    Plug 'jesseleite/vim-sourcery'
+    
+    " Surround
+    Plug 'tpope/vim-surround'
+
+    " Emmet
+    Plug 'mattn/emmet-vim'
+ 
+    " Telescope
+    Plug 'nvim-lua/plenary.nvim'
+    Plug 'nvim-telescope/telescope.nvim'
+
+    " Terminal
+    Plug 'voldikss/vim-floaterm'
+
+    " Git Diff
+    Plug 'sindrets/diffview.nvim'
+    Plug 'ThePrimeagen/git-worktree.nvim'
+
+    " Refactoring Tools
+    Plug 'phpactor/phpactor', {'for': 'php', 'do': 'composer install'}
+
+    " Git Signify
+    if has('nvim') || has('patch-8.0.902')
+        Plug 'mhinz/vim-signify'
+    else
+        Plug 'mhinz/vim-signify', { 'branch': 'legacy' }
+    endif
+
+    " Treesitter
+    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+    " Web Dev Icons
+    Plug 'kyazdani42/nvim-web-devicons'
+
+    " File Icons
+    Plug 'ryanoasis/vim-devicons'
+
+    " Lualine
+    Plug 'nvim-lualine/lualine.nvim'
+
+    " LSP
+    Plug 'neovim/nvim-lspconfig'
+    Plug 'williamboman/nvim-lsp-installer'
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/cmp-path'
+    Plug 'hrsh7th/cmp-cmdline'
+    Plug 'hrsh7th/nvim-cmp'
+    Plug 'nvim-lua/lsp-status.nvim'
+    Plug 'L3MON4D3/LuaSnip'
+    Plug 'saadparwaiz1/cmp_luasnip'
+    Plug 'folke/trouble.nvim'
+    Plug 'onsails/lspkind-nvim'
+    Plug 'tami5/lspsaga.nvim', { 'branch':'nvim6.0' }
+ 
+    " Iceberg Theme
+    Plug 'cocopon/iceberg.vim'
+    
+    " Indent Guides
+    Plug 'lukas-reineke/indent-blankline.nvim'
+    
+    " Buffer Line
+    Plug 'akinsho/bufferline.nvim'
+ 
+    " Smooth Scroll
+    Plug 'karb94/neoscroll.nvim'
+
+" ------------------------------------------------------------------------------
+" # Plugins Config
+" ------------------------------------------------------------------------------
+
+" Config: floaterm
+let g:floaterm_width=0.9
+let g:floaterm_height=0.9
+let floaterm_title='($1/$2)'
+
+" Config: indent-blankline
+let g:indent_blankline_filetype_exclude = ['dashboard', 'floaterm', 'nerdtree', 'lspinfo', 'lsp-installer' ]
+let g:indent_blankline_use_treesitter = v:true
+
+" Config: vim-signify
+let g:signify_priority = 1
+let g:signify_sign_add = '▍'
+let g:signify_sign_change = '▍'
+let g:signify_sign_delete_first_line = '▔'
+let g:signify_sign_delete = '▁'
+
+" Config: nerdtree
+let g:NERDTreeWinPos = "left"
+:let g:NERDTreeWinSize=35
+let g:NERDTreeGitStatusUseNerdFonts = 1
+let g:NERDTreeGitStatusIndicatorMapCustom = {
+                \ 'Modified'  :'✹',
+                \ 'Staged'    :'✚',
+                \ 'Untracked' :'✭',
+                \ 'Renamed'   :'➜',
+                \ 'Unmerged'  :'═',
+                \ 'Deleted'   :'✖',
+                \ 'Dirty'     :'~',
+                \ 'Ignored'   :'☒',
+                \ 'Clean'     :'✔︎',
+                \ 'Unknown'   :'?',
+                \ }
+
+let g:NERDTreeStatusline = '%#NonText#'
